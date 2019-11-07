418dfc20f9ed9281bf3d2f9141d9ac076556e3df
DeRuss404 <56159895+DeRuss404@users.noreply.github.com>
2019-11-07 01:54:29 +0000

    restored old dec2hex to fix doc

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -1,26 +1,13 @@
-//Daniel Russell
+import java.util.Scanner;
 
-//Imports
-//import java.util.Scanner;
-
-//Main class
 class Dec2Hex
 {
-    //declaration of class attributes
-    private static int Arg1;
-    //private static Scanner scr1;
 
-    //main method
-    public static void main(String[] args)
+    public static int Arg1;
+
+    public static void main(String args[])
     {
-        
-        //declaration of method attributes
-	char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-        int rem, num;
-        String hexadecimal=""; 
-        
-        //if condition to check size and type of input
-        if (args.length >= 1) 
+        if (args.length > 0) 
         {
             try 
             {
@@ -32,19 +19,13 @@ class Dec2Hex
                 System.exit(1);
             }
         }
-        else
-        {
-            System.err.println("NULL Argument, unable to convert to Hexadecimal");
-            System.exit(1);
-        }
 
-        //sets num after if check
+    	char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+         int rem, num;
 	num = Arg1;
-        
-        //console output confirming input
+	String hexadecimal=""; 
         System.out.println("Converting the Decimal Value " + num + " to Hex...");
         
-        //while conversion of decimal integer "num" into a hexadecimal represented as a string
         while(num != 0)
         {
 	    rem=num%16;
@@ -52,7 +33,6 @@ class Dec2Hex
             num= num/16;
         }
         
-        //final console confirming success
         System.out.println("Hexadecimal representation is : " + hexadecimal);
 
     }
